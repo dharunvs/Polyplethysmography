e69115c99b75771e6f732ac7f7086dfab530eaa5
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-05-18 12:26:07 +0530

    Updates

--- main.py
+++ main.py
@@ -1,67 +1,51 @@
-import os
-import numpy as np
-import imageio
-import matplotlib.pyplot as pt
+from tkinter import *
+from tkinter import filedialog
 
-fn = 'buji'
-videofile = f"test/{fn}.mp4"
-
-fps = 30
-PLOT = True
-SAVE = True
-
-pt.style.use("fivethirtyeight")
+# COLORS
+WHITE = '#FFFFFF'
+BLACK = '#000000'
+BLUE = '#0093FF'
+GREY_0 = '#D6D6D6'
+GREY_1 = '#C6C6C6'
 
 
-video = imageio.get_reader(videofile, 'ffmpeg')
+class App:
+    def __init__(self):
+        self.root = None
+        self.width = 500
+        self.height = 500
 
-colors = {
-    'red': [],
-    'green': [],
-    'blue': [],
-}
-
-for frame in video:
-    lumped_pixel = np.mean(frame, axis=(0,1))
-    colors['red'].append(lumped_pixel[0])
-    colors['green'].append(lumped_pixel[1])
-    colors['blue'].append(lumped_pixel[2])
-
-for key in colors:
-    colors[key] = np.divide(colors[key], 255)
-    # print(colors[key])
-
-x = np.arange(len(colors['red'])) / fps
-
-colors['red_filt'] = list()
-colors['red_filt'] = np.append(colors['red_filt'], colors['red'][0])
-
-tau= 0.25
-fsample = fps
-alpha = tau/ (tau + 2/fsample)
-
-for i, j in enumerate(colors['red']):
-    if i > 0:
-        y_prev = colors['red_filt'][i-1]
-        x_curr = colors['red'][i - 1]
-        x_prev = colors['red'][i]
-        colors['red_filt'] = np.append(colors['red_filt'], alpha*(y_prev + x_curr - x_prev))
-
-x_filt = x[50:-1]
-colors['red_filt'] = colors['red_filt'][50:-1]
+        self.file_path = ""
 
 
-red_fft = np.absolute(np.fft.fft(colors['red_filt']))
-N = len(colors['red_filt'])
-freqs = np.arange(0, fsample/2, fsample/N)
+    def run(self):
+        self.root = Tk()
+        self.root.geometry(f"{self.width}x{self.height}")
+        self.root.resizable(False, False)
+        self.root.title("Webpage Analyzer")
+        self.root.configure(bg=GREY_1)
 
-red_fft = red_fft[0:len(freqs)]
+        self.file_button = Button(self.root, text="Select video", command=self.get_file)
+        self.print_button = Button(self.root, text="Print", command=self.print_command)
 
-max_val = 0
-max_index = 0
-for index, fft_val in enumerate(red_fft):
-    if fft_val > max_val:
-        max_index = index
 
-heartrate = freqs[max_index] *60
-print(fn, heartrate/10)
+        self.file_button.pack()
+        self.print_button.pack()
+
+        self.root.mainloop()
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+    def get_file(self):
+        self.file_path = filedialog.askopenfile(initialdir="/")
+
+    def print_command(self):
+        for i in self.file_path:
+            print(i)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    App().run()
---
+++ reading.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+import imageio
+import matplotlib.pyplot as pt
+
+
+def value(filename, fps, name):
+    fn = filename
+    videofile = f"test/{fn}.mp4"
+
+    fps = fps
+    PLOT = True
+    SAVE = True
+
+    pt.style.use("fivethirtyeight")
+
+
+    video = imageio.get_reader(videofile, 'ffmpeg')
+
+    colors = {
+        'red': [],
+        'green': [],
+        'blue': [],
+    }
+
+    for frame in video:
+        lumped_pixel = np.mean(frame, axis=(0,1))
+        colors['red'].append(lumped_pixel[0])
+        colors['green'].append(lumped_pixel[1])
+        colors['blue'].append(lumped_pixel[2])
+
+    for key in colors:
+        colors[key] = np.divide(colors[key], 255)
+        # print(colors[key])
+
+    x = np.arange(len(colors['red'])) / fps
+
+    colors['red_filt'] = list()
+    colors['red_filt'] = np.append(colors['red_filt'], colors['red'][0])
+
+    tau= 0.25
+    fsample = fps
+    alpha = tau/ (tau + 2/fsample)
+
+    for i, j in enumerate(colors['red']):
+        if i > 0:
+            y_prev = colors['red_filt'][i-1]
+            x_curr = colors['red'][i - 1]
+            x_prev = colors['red'][i]
+            colors['red_filt'] = np.append(colors['red_filt'], alpha*(y_prev + x_curr - x_prev))
+
+    x_filt = x[50:-1]
+    colors['red_filt'] = colors['red_filt'][50:-1]
+
+
+    red_fft = np.absolute(np.fft.fft(colors['red_filt']))
+    N = len(colors['red_filt'])
+    freqs = np.arange(0, fsample/2, fsample/N)
+
+    red_fft = red_fft[0:len(freqs)]
+
+    max_val = 0
+    max_index = 0
+    for index, fft_val in enumerate(red_fft):
+        if fft_val > max_val:
+            max_index = index
+
+    heartrate = freqs[max_index] *60
+
+    return fn, heartrate/10
